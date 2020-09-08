d6da8c7689a656686399a95a865215f04b65588f
aknokd <aknokd.814@gmail.com>
2020-09-08 17:45:29 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :therapists
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
     registrations: 'users/registrations'
+  }
+  devise_for :therapists, controllers: {
+    sessions: 'therapists/sessions',
+    passwords: 'therapists/passwords',
+    registrations: 'therapists/registrations'
   }
 end

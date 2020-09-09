0f014d33226ddb6db52b4928fa29d64738e619ab
aknokd <aknokd.814@gmail.com>
2020-09-10 00:08:43 +0900

    user_informationsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_103853) do
+ActiveRecord::Schema.define(version: 2020_09_09_150118) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -70,6 +70,22 @@ ActiveRecord::Schema.define(version: 2020_09_09_103853) do
     t.index ["reset_password_token"], name: "index_therapists_on_reset_password_token", unique: true
   end
 
+  create_table "user_informations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "full_name", null: false
+    t.string "full_name_kana", null: false
+    t.string "gender", null: false
+    t.integer "age", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "address", null: false
+    t.string "building_name"
+    t.string "phone_number", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_user_informations_on_product_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
@@ -87,4 +103,5 @@ ActiveRecord::Schema.define(version: 2020_09_09_103853) do
   add_foreign_key "products", "therapists"
   add_foreign_key "reservations", "products"
   add_foreign_key "reservations", "users"
+  add_foreign_key "user_informations", "products"
 end

1641ac25766e91f3e021874a641e39cebdfacc1e
aknokd <aknokd.814@gmail.com>
2020-09-09 19:50:02 +0900

    アソシエーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :therapist
   has_one_attached :image
+  has_many :reservations
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :qualification

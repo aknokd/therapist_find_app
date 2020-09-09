16572cae3d4277a9b8dccf83c1555c8bfa97e46b
aknokd <aknokd.814@gmail.com>
2020-09-09 16:49:00 +0900

    アソシエーションの設定

--- app/models/therapist.rb
+++ app/models/therapist.rb
@@ -3,4 +3,6 @@ class Therapist < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :product
 end

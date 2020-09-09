10bad9796ff1458da7b16bc292d033685458826c
aknokd <aknokd.814@gmail.com>
2020-09-09 17:01:49 +0900

    newアクションとcreateアクションを追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,25 @@
 class ProductsController < ApplicationController
+  def index
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.valid?
+      @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:qualification_id, :prefecture_id, :self_introduction,
+                                    :price).merge(therapist_id: current_therapist.id)
+  end
 end

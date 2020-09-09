eeb49a1604d29c1976c5b0ed41504357066d3d40
aknokd <aknokd.814@gmail.com>
2020-09-09 19:29:36 +0900

    各アクションを追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_therapist!, except: [:index, :show]
+  before_action :ensure_correct_therapist, only: [:edit, :update, :destroy]
+
   def index
     @products = Product.all.includes(:therapist).order('created_at DESC')
   end
@@ -21,10 +24,35 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+    if @product.update_attributes(product_params)
+      redirect_to product_path(@product.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if current_therapist.id == @product.therapist.id
+      @product.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+
   private
 
   def product_params
     params.require(:product).permit(:image, :qualification_id, :prefecture_id, :self_introduction,
                                     :price).merge(therapist_id: current_therapist.id)
+  end
+
+  def ensure_correct_therapist
+    @product = Product.find(params[:id])
+    redirect_to root_path if current_therapist.id != @product.therapist.id
   end
 end

504519cc58ed215cd6890c473cc7ad09d50a93f0
aknokd <aknokd.814@gmail.com>
2020-09-10 11:20:41 +0900

    createアクションの修正

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,19 +2,25 @@ class ReservationsController < ApplicationController
   before_action :set_product
 
   def index
-    @reservation = Reservation.new
+    @reservation = ProductReservation.new
   end
 
   def create
-    @reservation = Reservation.new(reservation_params)
+    @reservation = ProductReservation.new(reservation_params)
     if @reservation.valid?
       @reservation.save
+      render :create
     else
       render :index
     end
   end
 
   private
+
+  def reservation_params
+    params.permit(:full_name, :full_name_kana, :gender, :age, :prefecture_id, :city,
+                  :address, :building_name, :phone_number, :datetime, :product_id).merge(user_id: current_user.id)
+  end
 
   def set_product
     @product = Product.find(params[:product_id])

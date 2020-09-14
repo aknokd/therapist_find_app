12f8d51fa7fd3ae831b1fafd9b3670210299d924
aknokd <aknokd.814@gmail.com>
2020-09-14 18:37:47 +0900

    セラピストログインをしている場合には予約画面には遷移できない

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,6 @@
 class ReservationsController < ApplicationController
   before_action :set_product
+  before_action :redirect_root
   before_action :authenticate_user!
 
   def index
@@ -27,4 +28,7 @@ class ReservationsController < ApplicationController
     @product = Product.find(params[:product_id])
   end
 
+  def redirect_root
+    redirect_to root_path if therapist_signed_in?
+  end
 end

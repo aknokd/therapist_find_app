c39dbd16cd0aad38d2255d950a2add61b0e94210
aknokd <aknokd.814@gmail.com>
2020-09-15 18:10:55 +0900

    userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   has_many :reservations
+
+  with_options presence: true do
+    validates :nickname
+    validates :email,                 uniqueness: { case_sensitive: true }
+    validates :password,              confirmation: true
+    validates :password_confirmation
+  end
 end

5b98cb23cd48d6001d2c3feae019e4e7e0ae5b8b
aknokd <aknokd.814@gmail.com>
2020-09-15 18:24:58 +0900

    productモデルのバリデーション設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,12 @@ class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :qualification
   belongs_to_active_hash :prefecture
+
+  with_options presence: true do
+    validates :image
+    validates :qualification_id,  numericality: { other_than: 0 }
+    validates :prefecture_id,     numericality: { other_than: 0 }
+    validates :self_introduction
+    validates :price
+  end
 end
